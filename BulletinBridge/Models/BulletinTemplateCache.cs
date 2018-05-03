cb428e6f3e3e54d2cea095e484aafcfbd6c021d1
Fess <Kozhevnikov.A.M@yandex.ru>
2018-05-03 13:37:06 +0300

    ~fix10

---
+++ BulletinBridge/Models/BulletinTemplateCache.cs
@@ -0,0 +1,36 @@
+﻿using FessooFramework.Objects.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinBridge.Models
+{
+    [DataContract]
+    public class BulletinTemplateCache : CacheObject
+    {
+        [DataMember]
+        public string URL { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Price { get; set; }
+        /// <summary>
+        /// Количество просмотров
+        /// </summary>
+        [DataMember]
+        public string Count { get; set; }
+
+        [DataMember]
+        public string Images { get; set; }
+        [DataMember]
+        public string GroupSignature { get; set; }
+        [DataMember]
+        public string Region { get; set; }
+
+    }
+}
---
+++ BulletinEngine/Models/BulletinTemplate.cs
@@ -0,0 +1,83 @@
+﻿using BulletinBridge.Models;
+using FessooFramework.Objects.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinHub.Models
+{
+    public class BulletinTemplate : EntityObjectALM<BulletinTemplate, DefaultState>
+    {
+        #region Property
+        public string URL { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        /// <summary>
+        /// Количество просмотров
+        /// </summary>
+        public string Count { get; set; }
+
+        public string Images { get; set; }
+        public string GroupSignature { get; set; }
+        /// <summary>
+        /// Область >> Город >> Место
+        /// </summary>
+        public string Region { get; set; }
+        #endregion
+        #region ALM
+        protected override IEnumerable<EntityObjectALMConfiguration<BulletinTemplate, DefaultState>> Configurations => Enumerable.Empty<EntityObjectALMConfiguration<BulletinTemplate, DefaultState>>();
+
+        protected override int GetStateValue(DefaultState state)
+        {
+            return (int)state;
+        }
+
+        protected override BulletinTemplate SetValueDefault(BulletinTemplate arg1, BulletinTemplate arg2)
+        {
+            arg1.URL = arg2.URL;
+            arg1.Title = arg2.Title;
+            arg1.Description = arg2.Description;
+            arg1.Price = arg2.Price;
+            arg1.Count = arg2.Count;
+            arg1.Images = arg2.Images;
+            arg1.GroupSignature = arg2.GroupSignature;
+            arg1.Region = arg2.Region;
+            return arg1;
+        }
+        #endregion
+        #region Creators
+        protected override IEnumerable<EntityObjectALMCreator<BulletinTemplate>> CreatorsService => new[]
+        {
+            EntityObjectALMCreator<BulletinTemplate>.New<BulletinTemplateCache>(ToCache, ToEntity, new Version(1,0,0,0))
+        };
+        private BulletinTemplate ToEntity(BulletinTemplateCache cache, BulletinTemplate entity)
+        {
+            entity.URL = cache.URL;
+            entity.Title = cache.Title;
+            entity.Description = cache.Description;
+            entity.Price = cache.Price;
+            entity.Count = cache.Count;
+            entity.Images = cache.Images;
+            entity.GroupSignature = cache.GroupSignature;
+            entity.Region = cache.Region;
+            return entity;
+        }
+        internal static BulletinTemplateCache ToCache(BulletinTemplate entity)
+        {
+            var cache = new BulletinTemplateCache();
+            cache.URL = entity.URL;
+            cache.Title = entity.Title;
+            cache.Description = entity.Description;
+            cache.Price = entity.Price;
+            cache.Count = entity.Count;
+            cache.Images = entity.Images;
+            cache.GroupSignature = entity.GroupSignature;
+            cache.Region = entity.Region;
+            return cache;
+        }
+        #endregion
+    }
+}
