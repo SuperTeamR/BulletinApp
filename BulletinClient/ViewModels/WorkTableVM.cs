456afb3ade4ad37de48fb815757cdd129193ddf0
Sergei Milovanov <SVMilovanov@1cbit.ru>
2018-05-04 16:11:49 +0300

    Fix

--- BulletinClient/ViewModels/WorkTableVM.cs
+++ BulletinClient/ViewModels/WorkTableVM.cs
@@ -26,9 +26,9 @@ namespace BulletinClient.ViewModels
         public AccessCollectionVM accessCollectionView { get; set; }
         public BulletinCollectionVM BulletinCollectionView => bulletinCollectionView = bulletinCollectionView ?? new BulletinCollectionVM();
         public BulletinCollectionVM bulletinCollectionView { get; set; }
-        public TemplateCollectionVM TemplateCollectionView =>
-            templateCollectionView = templateCollectionView ?? new TemplateCollectionVM();
-        public TemplateCollectionVM templateCollectionView { get; set; }
+        //public TemplateCollectionVM TemplateCollectionView =>
+        //    templateCollectionView = templateCollectionView ?? new TemplateCollectionVM();
+        //public TemplateCollectionVM templateCollectionView { get; set; }
 
         public bool Bulletin => Bulletins != null && Bulletins.Any();
         public bool NotBulletin => Bulletins == null || !Bulletins.Any();
